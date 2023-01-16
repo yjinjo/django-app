6df278a0dc0fbc98cf219bf2d0b03c3a9213b91f
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-16 11:10:57 +0900

    #5. Logins

--- fifth_project/basic_app/urls.py
+++ fifth_project/basic_app/urls.py
@@ -4,4 +4,10 @@ from basic_app import views
 # Template urls
 app_name = "basic_app"
 
-urlpatterns = [path("register/", views.register, name="register")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("register/", views.register, name="register"),
+    path("user_login/", views.user_login, name="user_login"),
+    path("logout/", views.user_logout, name="logout"),
+    path("special/", views.special, name="special"),
+]
--- fifth_project/fifth_project/settings.py
+++ fifth_project/fifth_project/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = "media/"
 
+LOGIN_URL = "basic_app/user_login"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
