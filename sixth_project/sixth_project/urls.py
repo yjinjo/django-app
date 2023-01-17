bfd6f10a08c209ef0af3f55701fa13c38bebdaad
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-17 10:56:03 +0900

    #7. Template Views with CBV

--- sixth_project/sixth_project/urls.py
+++ sixth_project/sixth_project/urls.py
@@ -20,5 +20,5 @@ from basic_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("", views.index, name="index"),
-    path("", views.CBView.as_view()),
+    path("", views.IndexView.as_view()),
 ]
