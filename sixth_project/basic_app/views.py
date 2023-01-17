f0f2c14799aac62c37fc40ec17bdbf0e1b34cadb
Youngjin Jo <yjinjo@berkeley.edu>
2023-01-17 12:37:02 +0900

    #7. Update Detail View and List View

--- sixth_project/basic_app/views.py
+++ sixth_project/basic_app/views.py
@@ -3,11 +3,17 @@ from django.views.generic import View, TemplateView, ListView, DetailView
 from . import models
 
 
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
 class SchoolListView(ListView):
+    context_object_name = "schools"
     # You will be provided with a list of every record
     model = models.School
 
 
 class SchoolDetailView(DetailView):
+    context_object_name = "school_detail"
     model = models.School
     template_name = "basic_app/school_detail.html"
